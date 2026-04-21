8e3e147246c749f1c9a96261ad0079d6feb4bc03
LosSixtySix <jonathanhoyt0523@gmail.com>
2026-04-21 15:47:52 -0600

    first commit

--- test.pl
+++ test.pl
@@ -1,18 +1,16 @@
-
-:- use_module(customers).
-test_save(Fact) :-
-    open('test.txt',append,Stream),
-    write(Stream,Fact),
-    write(Stream,'.'),
-    nl(Stream),
-    close(Stream).
-
-test_tell(Fact) :-
-    tell('test.txt'),
-    write(Fact),
-    nl,
-    told.
-
-test_save2(Fact) :-
-    forall(test_save(Fact),true).
-
+customer(1,jill,"jil@gmail.com",4566454555,true).
+customer(2,joebob,"test@gmail.com",456664646,false).
+customer(4,george,"yourmom",4356667777,true).
+customer(13,test,"test",test,true).
+customer(14,test,"test",test,true).
+customer(15,test,"test",test,true).
+customer(16,test,"test",test,true).
+customer(17,test,"test",test,true).
+customer(18,test,"test",test,true).
+customer(19,test,"test",test,true).
+customer(20,test,"test",test,true).
+customer(21,test,"test",test,true).
+customer(22,test,"test",test,true).
+customer(23,test3,"tet54",t,false).
+customer(0,jane,"jane@gmail.com",111111111,true).
+customer(24,jane,'joebob@gmail.com',1112223333,true).
